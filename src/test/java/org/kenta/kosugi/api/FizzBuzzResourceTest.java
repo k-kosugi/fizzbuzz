c31e042515f1f13d456e84d6f15180418dbc1d38
kentak <kenta.kosugi@gmail.com>
2019-06-18 16:23:56 +0900

    Fizzが3の倍数から5の倍数に更新されたのでソースに反映。

--- src/test/java/org/kenta/kosugi/api/FizzBuzzResourceTest.java
+++ src/test/java/org/kenta/kosugi/api/FizzBuzzResourceTest.java
@@ -1,162 +1,36 @@
 package org.kenta.kosugi.api;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class FizzBuzzResourceTest {
 
-    @Test
-    public void testIsFizz001() {
-        var test = new FizzBuzzResource();
+    @Before
+    public void setUp() throws Exception {
+    }
 
-        // 3は3の倍数なのでisFizz()はtrue
-        assertTrue(test.isFizz(3));
+    @After
+    public void tearDown() throws Exception {
     }
 
     @Test
-    public void testIsFizz002() {
-        var test = new FizzBuzzResource();
-
-        // 15は3の倍数なのでisFizz()はtrue
-        assertTrue(test.isFizz(15));
+    public void isFizz001() {
+        var target = new FizzBuzzResource();
+        assertTrue(target.isFizz(3));
     }
 
     @Test
-    public void testIsFizz003() {
-        var test = new FizzBuzzResource();
-
-        // 0は3の倍数(0倍)なのでisFizz()はtrue
-        assertTrue(test.isFizz(0));
+    public void isBuzz() {
     }
 
     @Test
-    public void testIsFizz004(){
-        var test = new FizzBuzzResource();
-
-        // 1は3の倍数ではないのでisFizz()はfalse
-        assertFalse(test.isFizz(1));
+    public void isFizzBuzz() {
     }
 
     @Test
-    public void testIsFizz005(){
-        var test = new FizzBuzzResource();
-
-        // 5は3の倍数ではないのでisFizz()はfalse
-        assertFalse(test.isFizz(5));
-    }
-
-    @Test
-    public void testIsFizz006(){
-        var test = new FizzBuzzResource();
-
-        // -1は3の倍数ではないのでisFizz()はfalse
-        assertFalse(test.isFizz(-1));
-    }
-
-    @Test
-    public void testIsFizz007(){
-        var test = new FizzBuzzResource();
-
-        // 14は3の倍数ではないのでisFizz()はfalse
-        assertFalse(test.isFizz(14));
-    }
-
-    @Test
-    public void testIsFizz008(){
-        var test = new FizzBuzzResource();
-
-        // 16は3の倍数ではないのでisFizz()はfalse
-        assertFalse(test.isFizz(16));
-    }
-
-    @Test
-    public void testIsBuzz001(){
-        var test = new FizzBuzzResource();
-
-        // 5は5の倍数なのでisBuzz()はtrue
-        assertTrue(test.isBuzz(5));
-    }
-
-    @Test
-    public void testIsBuzz002(){
-        var test = new FizzBuzzResource();
-
-        // 0は5の倍数なのでisBuzz()はtrue
-        assertTrue(test.isBuzz(0));
-    }
-
-
-    @Test
-    public void testIsBuzz003(){
-        var test = new FizzBuzzResource();
-
-        // 15は5の倍数なのでisBuzz()はtrue
-        assertTrue(test.isBuzz(15));
-    }
-
-    @Test
-    public void testIsBuzz004(){
-        var test = new FizzBuzzResource();
-
-        // -1は5の倍数ではないのでisBuzz()はfalse
-        assertFalse(test.isBuzz(-1));
-    }
-
-    @Test
-    public void testIsBuzz005(){
-        var test = new FizzBuzzResource();
-
-        // 4は5の倍数ではないのでisBuzz()はfalse
-        assertFalse(test.isBuzz(4));
-    }
-
-    @Test
-    public void testIsBuzz006(){
-        var test = new FizzBuzzResource();
-
-        // 6は5の倍数ではないのでisBuzz()はfalse
-        assertFalse(test.isBuzz(6));
-    }
-
-
-    @Test
-    public void testIsBuzz007(){
-        var test = new FizzBuzzResource();
-
-        // 14は5の倍数ではないのでisBuzz()はfalse
-        assertFalse(test.isBuzz(14));
-    }
-
-    @Test
-    public void testIsBuzz008(){
-        var test = new FizzBuzzResource();
-
-        // 16は5の倍数ではないのでisBuzz()はfalse
-        assertFalse(test.isBuzz(16));
-    }
-
-    @Test
-    public void testIsFizzBuzz001(){
-        var test = new FizzBuzzResource();
-
-        // 0は3と5の倍数なのでisFizzBuzz()はtrue
-        assertTrue(test.isFizzBuzz(0));
-    }
-
-    @Test
-    public void testIsFizzBuzz002(){
-        var test = new FizzBuzzResource();
-
-        // 15は3と5の倍数なのでisFizzBuzz()はtrue
-        assertTrue(test.isFizzBuzz(15));
-    }
-
-    @Test
-    public void testIsFizzBuzz003(){
-        var test = new FizzBuzzResource();
-
-        // -15は3と5の倍数なのでisFizzBuzz()はtrue
-        assertTrue(test.isFizzBuzz(-15));
+    public void fizzbuzz() {
     }
 }
